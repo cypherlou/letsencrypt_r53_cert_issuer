892ebc2f702486664a58c3e08c1732f37c0a186e
glennstar <cypherlou666@gmail.com>
2021-03-07 20:55:00 +0000

    reformatted pre-commit

--- ic/cert.py
+++ ic/cert.py
@@ -9,8 +9,10 @@ import socket
 import datetime
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class Cert( object ):
-    def __init__( self, server:str, logger )->None:
+
+
+class Cert(object):
+    def __init__(self, server: str, logger) -> None:
         self.log = logger
         self.server = server
         self.port = 443
@@ -18,70 +20,81 @@ class Cert( object ):
         self.expires_in = 0
         self.error = True
 
-    def get_expiration( self, domain:str ):
+    def get_expiration(self, domain: str):
 
         if not self.server:
             self.server = domain
 
         context = ssl.create_default_context()
-        self.log.info( "connecting to {}:{}".format( self.server, self.port) )
+        self.log.info("connecting to {}:{}".format(self.server, self.port))
 
         self.error = True
         try:
             sock = socket.create_connection((self.server, self.port))
         except Exception as e:
-            self.log.error( e )
-            return 
+            self.log.error(e)
+            return
 
-        self.log.info( "getting certificate for {}".format( domain ) )
+        self.log.info("getting certificate for {}".format(domain))
         try:
             ssl_socket = context.wrap_socket(sock, server_hostname=domain)
         except Exception as e:
-            self.log.error( e )
+            self.log.error(e)
             return
-        
+
         der_cert = ssl_socket.getpeercert(True)
         pem_cert = ssl.DER_cert_to_PEM_cert(der_cert)
 
-        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, pem_cert)
-        self.expiration_date = datetime.datetime.strptime( x509.get_notAfter().decode(), '%Y%m%d%H%M%SZ' )
+        x509 = OpenSSL.crypto.load_certificate(
+            OpenSSL.crypto.FILETYPE_PEM, pem_cert)
+        self.expiration_date = datetime.datetime.strptime(
+            x509.get_notAfter().decode(), '%Y%m%d%H%M%SZ')
         self.expires_in = (self.expiration_date - datetime.datetime.now()).days
-        self.log.info( "certificate expires on {}, {} days away".format( self.expiration_date, self.expires_in ) )
+        self.log.info(
+            "certificate expires on {}, {} days away".format(
+                self.expiration_date,
+                self.expires_in))
         self.error = False
-    
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class IssueCert( object ):
 
-    def __init__( self, logger ):
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+class IssueCert(object):
+
+    def __init__(self, logger):
 
         self.log = logger
         self.domain = None
         self.output_pem = False
         self.key_file = '.lets_encrypt.pem'
         self.account_key = None
-        self.key_type='rsa3072'
+        self.key_type = 'rsa3072'
         self.email = ''
         self.new_account = True
         self.error = True
 
     # -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
-    def issue_cert( self, domain:str ) -> None:
+    def issue_cert(self, domain: str) -> None:
 
-        if not os.environ.get( 'AWS_SECRET_ACCESS_KEY' ) or not os.environ.get( 'AWS_ACCESS_KEY_ID' ):
-            self.log.warning( "AWS_SECRET_ACCESS_KEY and/or AWS_ACCESS_KEY_ID not set. Sewer uses environment variables to access AWS R53" )
+        if not os.environ.get('AWS_SECRET_ACCESS_KEY') or not os.environ.get(
+                'AWS_ACCESS_KEY_ID'):
+            self.log.warning(
+                "AWS_SECRET_ACCESS_KEY and/or AWS_ACCESS_KEY_ID not set. Sewer uses environment variables to access AWS R53")
             return
 
-        filename, extension = os.path.splitext( self.key_file )
-        self.account_key_file = "{}_account{}".format( filename, extension )
-        self.certificate_key_file = "{}_certificate{}".format( filename, extension )
-        
-        if os.path.exists( self.key_file ):
-            self.log.info( "loading Let's Encrypt account keys" )
-            self.account = sewer.crypto.AcemAccount.read_pem(self.account_key_file)
+        filename, extension = os.path.splitext(self.key_file)
+        self.account_key_file = "{}_account{}".format(filename, extension)
+        self.certificate_key_file = "{}_certificate{}".format(
+            filename, extension)
+
+        if os.path.exists(self.key_file):
+            self.log.info("loading Let's Encrypt account keys")
+            self.account = sewer.crypto.AcemAccount.read_pem(
+                self.account_key_file)
             self.certificate = sewer.crypto.read_pem(self.certificate_key_file)
             self.new_account = False
         else:
-            self.log.info( "creating Let's Encrypt account keys" )
+            self.log.info("creating Let's Encrypt account keys")
             self.account = sewer.crypto.AcmeAccount.create(self.key_type)
             self.certificate = sewer.crypto.AcmeKey.create(self.key_type)
 
@@ -93,32 +106,41 @@ class IssueCert( object ):
             self.naked_domain = domain
 
         if not self.email:
-            self.email = "info@{}".format( self.naked_domain )
-            
-        self.log.info( "making a cert request for {}".format( self.naked_domain ) )
+            self.email = "info@{}".format(self.naked_domain)
+
+        self.log.info("making a cert request for {}".format(self.naked_domain))
 
         try:
             client = sewer.client.Client(
-                domain_name = "*.{}".format( self.naked_domain ),
-                domain_alt_names=[ self.naked_domain ],
-                contact_email = self.email,
-                provider = dns_provider,
+                domain_name="*.{}".format(self.naked_domain),
+                domain_alt_names=[self.naked_domain],
+                contact_email=self.email,
+                provider=dns_provider,
                 # LOG_LEVEL = "DEBUG",
-                LOG_LEVEL = "INFO",
+                LOG_LEVEL="INFO",
                 account=self.account,
                 cert_key=self.certificate,
-                is_new_acct = self.new_account,
+                is_new_acct=self.new_account,
             )
 
         except Exception as e:
-            self.log.warning( "failed to request certificate from Let's Encrypt server: {}".format( e ) )
+            self.log.warning(
+                "failed to request certificate from Let's Encrypt server: {}".format(e))
             return
 
-        self.log.info("requesting certificate for {}".format(self.naked_domain) )
-        self.pem = "{}\n{}".format( client.get_certificate(), self.certificate.to_pem().decode() )
+        self.log.info(
+            "requesting certificate for {}".format(
+                self.naked_domain))
+        self.pem = "{}\n{}".format(
+            client.get_certificate(),
+            self.certificate.to_pem().decode())
 
-        self.log.info( "writing account credentials to {}".format( self.account_key_file) )
-        self.account.write_pem( self.account_key_file )
-        self.log.info( "writing certificate key to {}".format( self.certificate_key_file ) )
-        self.certificate.write_pem( self.certificate_key_file )
+        self.log.info(
+            "writing account credentials to {}".format(
+                self.account_key_file))
+        self.account.write_pem(self.account_key_file)
+        self.log.info(
+            "writing certificate key to {}".format(
+                self.certificate_key_file))
+        self.certificate.write_pem(self.certificate_key_file)
         self.error = False
